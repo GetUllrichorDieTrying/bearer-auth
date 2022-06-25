5d8d0c307e93cbee4e889e75104dd7d5430c388f
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-06-25 14:19:20 -0700

    server running

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -1,17 +1,24 @@
 'use strict';
 
+require('dotenv').config();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.SECRET || 'TEST_SECRET';
 
 const userSchema = (sequelize, DataTypes) => {
   const model = sequelize.define('User', {
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false, },
+    password: { type: DataTypes.STRING, allowNull: false },
     token: {
       type: DataTypes.VIRTUAL,
       get() {
-        return jwt.sign({ username: this.username });
-      }
-    }
+        return jwt.sign({ username: this.username }, SECRET);
+      },
+      set(payload) {
+        return jwt(payload, SECRET);
+      },
+    },
   });
 
   model.beforeCreate(async (user) => {
@@ -19,27 +26,31 @@ const userSchema = (sequelize, DataTypes) => {
     user.password = hashedPass;
   });
 
-  // Basic AUTH: Validating strings (username, password) 
+  // Basic AUTH: Validating strings (username, password)
   model.authenticateBasic = async function (username, password) {
-    const user = await this.findOne({ username })
-    const valid = await bcrypt.compare(password, user.password)
-    if (valid) { return user; }
+    const user = await this.findOne({ username });
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid) {
+      return user;
+    }
     throw new Error('Invalid User');
-  }
+  };
 
   // Bearer AUTH: Validating a token
   model.authenticateToken = async function (token) {
     try {
-      const parsedToken = jwt.verify(token, process.env.SECRET);
-      const user = this.findOne({ username: parsedToken.username })
-      if (user) { return user; }
-      throw new Error("User Not Found");
+      const parsedToken = jwt.verify(token, SECRET);
+      const user = this.findOne({ where: { username: parsedToken.username } });
+      if (user) {
+        return user;
+      }
+      throw new Error('User Not Found');
     } catch (e) {
-      throw new Error(e.message)
+      throw new Error(e.message);
     }
-  }
+  };
 
   return model;
-}
+};
 
 module.exports = userSchema;
